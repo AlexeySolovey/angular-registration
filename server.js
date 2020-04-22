03a25c79041616a379520184e79058a9638c130a
alexei <soloveyalexey3@gmail.com>
2020-04-22 16:51:55 +0300

    change files location

---
+++ server.js
@@ -0,0 +1,23 @@
+// https://www.youtube.com/watch?v=qML_nlIEnRQ&list=PLC3y8-rFHvwg2RBz6UplKTGIXREj9dV0G&index=23
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 80
+const api = require("./routes/api");
+const app = express();
+
+// app.use(express.static('ngApp'));
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use("/api", api);
+
+app.get("/", (req, res) => {
+  res.send("hello server1");
+});
+
+app.listen(PORT, function() {
+  console.log("Server running on localhost:" + PORT);
+});
