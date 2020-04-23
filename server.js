ba2569edc4238811e89b25f6256ac46d7aa035ac
alexei <soloveyalexey3@gmail.com>
2020-04-23 13:37:06 +0300

    last chance

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 4444;
 const api = require("./routes/api");
 const app = express();
 
-// app.use(express.static('ngApp'));
+app.use(express.static('ngApp'));
 
 app.use(cors());
 
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use("/api", api);
 
 app.get("/", (req, res) => {
-  res.send("hello server1");
+  res.sendFile(path.join(__dirname+'/ngApp/index.html'));
 });
 
 app.listen(PORT, function() {
